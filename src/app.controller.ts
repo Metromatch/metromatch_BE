5976640077137ce0d4ce036573a45657a85420bf
VishalTP <tpvishal96@gmail.com>
2026-06-13 19:17:16 +0530

    unnecessary

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('cat')
+@ApiTags('health')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-  @Get('poop')
-  getPoop(): string {
-    return this.appService.getHello('poop');
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-// @Injectable()
+@Injectable()
 export class AppService {
-  getHello(text: string | null = 'World'): string {
-    return 'Hello ' + text;
+  getHello(): string {
+    return 'Metromatch API is running!';
   }
 }
